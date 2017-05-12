74ff8264316051a9b4b50716e06ae8da7b4453e9
ksesalebangim <flowr3direction@gmail.com>
2017-05-12 23:31:59 +0300

    aworking sequence functionality

--- sequence_loader.lua
+++ sequence_loader.lua
@@ -1,15 +1,78 @@
-Json = require("json")
+local Json = require("json")
+require "torch"
+local model_loader = require "model_loader"
 
 local M = {}
-local function loadSequence()
+local currentEffectIndex = 0
+local currentEffect = nil
+local models = {}
+local sequence = {}
+local timer = torch.Timer()
+
+local function loadModels(modelsObject)
+  local model_paths = {}
+  for _,v in pairs(modelsObject) do
+    model_paths[v.name] = v.path
+  end
+  local models = {}
+  for k,v in pairs(model_paths) do
+    models[k] = model_loader.load_model(v)
+  end
+
+  return models
+end
+
+local function init()
   local fileName = 'sequence.json'
   local file = io.open(fileName, "r")
   if file then
     local content = file:read("*a")
     local lines = Json.decode(content)
     io.close(file)
-    return lines
+    models = loadModels(lines["models"])
+    sequence = lines["sequence"]
   end
 end
 
-M.loadSequence = loadSequence;
+local function next()
+  timer:reset()
+  currentEffectIndex = (currentEffectIndex+1) % #sequence
+  currentEffect = sequence[currentEffectIndex+1]
+  print("Moving to " .. currentEffect.name)
+end
+
+local function getCurrent()
+  currentEffect = sequence[currentEffectIndex+1]
+  return models[currentEffect.name], currentEffect.fadeIn, currentEffect.duration, currentEffect.strength
+end
+
+local function getNext()
+  currentEffect = sequence[((currentEffectIndex + 1) % #sequence) + 1]
+  return models[currentEffect.name], currentEffect.fadeIn, currentEffect.duration, currentEffect.strength
+end
+
+-- return [model 1, model 2, blend factor between model1 and model2 , blend factor of result with original camera feed]
+local function get_models()
+    local current, _, duration, strength = getCurrent()
+    local next_model, fade_in, _, next_strength = getNext()
+    local curr_time = timer:time().real
+    if (curr_time > (duration + fade_in)) then
+        next()
+        return get_models()
+    end 
+
+    -- simple case: only one model
+    if (curr_time <= duration) then
+	return current, current, 1.0, strength
+    end
+
+    -- complex case: blend between current and next
+    local t = (curr_time - duration) / fade_in
+    local factor = 1.0 - t
+     return current, next_model, factor, strength*factor + (1-factor)*next_strength
+end
+
+M.init = init
+M.next = next
+M.get_models = get_models
+return M
