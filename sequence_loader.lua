7731b2ef32772ce40e98fb4294dab2207b7c6359
Roee Shenberg <shenberg@gmail.com>
2017-05-24 12:06:31 +0300

    improved next effect mechanism

--- sequence_loader.lua
+++ sequence_loader.lua
@@ -1,5 +1,6 @@
 local Json = require("json")
 require "torch"
+require "math"
 local model_loader = require "model_loader"
 
 local M = {}
@@ -9,7 +10,8 @@ local targetEffectIndex = 0
 local sequence = {}
 local timer = torch.Timer()
 local manual_mode = false
-local user_change_timer = torch.Timer()
+
+local current_fade = nil
 -- TODO: dynamic
 local cooldown = 10
 
@@ -26,6 +28,50 @@ local function load_models(models_object)
   return models
 end
 
+
+local function create_fade(src_index, dst_index)
+  local src_effect = sequence[src_index + 1]
+  local dst_effect = sequence[dst_index + 1]
+  local fade = {
+                src_model = models[src_effect.name],
+                dst_model = models[dst_effect.name],
+                src_strength = src_effect.strength,
+                dst_strength = dst_effect.strength,
+                dst_index = dst_index,
+                duration = src_effect.duration,
+                fade_time = dst_effect.fadeIn,
+                next_fades = 0, -- for manual fade, how many will follow
+               }
+
+  return fade
+end
+
+local function fade_factor(fade)
+  local t = timer:time().real
+  if t < fade.duration then
+    -- 100% first effect
+    return 0
+  end
+
+  return math.min(1.0,  (t - fade.duration) / fade.fade_time)
+end
+
+local function is_fade_running(fade)
+  return timer:time().real < (fade.duration + fade.fade_time)
+end
+
+local function is_fade_fading(fade)
+  return timer:time().real >= fade.duration
+end
+
+local function is_fade_done(fade)
+  return timer:time().real >= (fade.duration + fade.fade_time)
+end
+
+local function is_fade_manual(fade)
+  return fade.next_fades > 0
+end
+
 local function next_index(idx)
   return (idx + 1) % #sequence
 end
@@ -39,6 +85,7 @@ local function init(fileName, sequence_name)
     models = load_models(lines["models"])
     sequence = lines[sequence_name]
     targetEffectIndex = next_index(currentEffectIndex)
+    current_fade = create_fade(currentEffectIndex, targetEffectIndex)
   end
 end
 
@@ -51,21 +98,21 @@ local function set_manual_mode(is_manual_mode)
   end
 end
 
-local function get_current()
-  local currentEffect = sequence[currentEffectIndex+1]
-  return models[currentEffect.name], currentEffect.fadeIn, currentEffect.duration, currentEffect.strength
-end
-
-local function get_next()
-  local currentEffect = sequence[targetEffectIndex + 1]
-  return models[currentEffect.name], currentEffect.fadeIn, currentEffect.duration, currentEffect.strength
-end
-
 local function go_to_next()
   timer:reset()
-  currentEffectIndex = targetEffectIndex
-  targetEffectIndex = next_index(targetEffectIndex)
+  --currentEffectIndex = targetEffectIndex
+  currentEffectIndex = current_fade.dst_index
+  targetEffectIndex = next_index(currentEffectIndex)
   local currentEffect = sequence[currentEffectIndex+1]
+  local next_fade = create_fade(currentEffectIndex, targetEffectIndex)
+  -- if this is a regular effect transition, use next_fade as-is
+  -- otherwise specially create the next fade to start immediately
+  if (not is_manual_mode) and (current_fade.next_fades > 1) then
+    print("Done with short manual fade, creating long manual fade")
+    next_fade.duration = 0
+    next_fade.next_fades = current_fade.next_fades - 1
+  end
+  current_fade = next_fade
   print("Moving to " .. currentEffect.name)
 end
 
@@ -73,59 +120,47 @@ local function next()
   if is_manual_mode then
     go_to_next()
   else
-    if user_change_timer:time().real < cooldown then
-      print("denied next due to cooldown: " .. user_change_timer:time().real .. "/" .. cooldown)
+    if is_fade_manual(current_fade) and is_fade_running(current_fade) then
+      print("denied next due to manual fade (" .. current_fade.next_fades .. "): " 
+            .. timer:time().real .. "/" .. current_fade.duration + current_fade.fade_time)
       return
     end
-    user_change_timer:reset()
 
     timer:stop()
-    local _, _, duration = get_current()
-    local _, fade_in = get_next()
-    local curr_time = timer:time().real
-    -- simple case: not in mid transition
-    if curr_time <= duration then
-      targetEffectIndex = next_index(targetEffectIndex)
-      print("Target changed: " .. sequence[targetEffectIndex+1].name)
-    elseif curr_time >= duration + fade_in then
-      print("shouldn't be here")
-      go_to_next()
+    if not is_fade_fading(current_fade) then
+      print("User-Next: forcing fade startt")
+      -- simple case - not fading.
+      -- immediately start fading
+      current_fade.duration = 0
+      current_fade.next_fades = 1 -- mark as manual fade
     else
-      -- tricky case: mid-blend, change the target
-    end
-    timer:resume()
-    print("Moving to " .. "crap")
-  end
-end
+      print("User-Next: forcing double-fade")
+      -- fix up current fade to be very fast, mark it so that an immediate-fade will start afterwards
+      local factor = 1.0 - fade_factor(current_fade)
+      current_fade.src_strength = current_fade.src_strength*factor + current_fade.dst_strength*(1 - factor)
 
-local function user_next()
+      current_fade.duration = 0
+      -- TODO: make a parameter 
+      current_fade.fade_time = 1 -- 1 sec for initial fade 
+
+      -- mark as manual, with one fade following it immediately
+      current_fade.next_fades = 2
+    end
+    timer:reset()
+    timer:resume()
+  end
 end
 
 -- return [model 1, model 2, blend factor between model1 and model2 , blend factor of result with original camera feed]
 local function get_models()
-  local current, _, duration, strength = get_current()
-  local next_model, fade_in, _, next_strength = get_next()
-  local curr_time = timer:time().real
-  if (not manual_mode) and (curr_time >= (duration + fade_in)) then
-      go_to_next()
-      return get_models()
+  if is_fade_done(current_fade) then
+    go_to_next()
+    return get_models()
   end
-
-  -- simple case: only one model
-  if (curr_time <= duration) then
-    return current, current, 1.0, strength
-  end
-
-  -- complex case: blend between current and next
-  local t = (curr_time - duration) / fade_in
-  local factor = 1.0 - t
-  
-  if factor > 1.0 then
-    factor = 1.0
-  elseif factor < 0 then
-    factor = 0
-  end
-  return current, next_model, factor, strength*factor + (1-factor)*next_strength
+  -- fade_factor returns 0 = src, 1 = dst,
+  -- invert that for blend strength
+  local factor = 1 - fade_factor(current_fade)
+  return current_fade.src_model, current_fade.dst_model, factor, current_fade.src_strength*factor + current_fade.dst_strength*(1 - factor)
 end
 
 M.init = init
